44386649691ff725133bf53c294c58f0c270801f
Francisco Gaspar Audino Re <faudre755@g.educaand.es>
2024-12-15 18:30:10 +0100

    putConcesionario

--- src/routes/concesionarios/concesionarios.js
+++ src/routes/concesionarios/concesionarios.js
@@ -24,4 +24,13 @@ router.get("/:id", (request, response) => {
     response.json({concesionario});
 });
 
+router.put("/:id", (request, response) => {
+    const id = parseInt(request.params.id);
+    const concesionario = concesionarios.find((c) => c.id === id);
+    const concesionarioAct = {
+        ...concesionarios[concesionario], ...request.body;
+    };
+    response.json({message: "ok"});
+})
+
 module.exports = router;
