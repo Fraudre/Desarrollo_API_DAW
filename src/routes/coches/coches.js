81a8d09bcb78754669ecd0217632335ef9d0a472
Francisco Gaspar Audino Re <faudre755@g.educaand.es>
2024-12-15 19:29:46 +0100

    getUnCoche

--- src/routes/coches/coches.js
+++ src/routes/coches/coches.js
@@ -11,14 +11,25 @@ router.get("/", (request, response) => {
 });
 
 router.post("/", (request, response) => {
-    const concesionarioId = parseInt(request.baseUrl.split("/").slice(-2, -1)[0]);
-    const concesionario = concesionarios.find((c) => c.id === concesionarioId);
-    const nuevoCoche = {
-      id: concesionarios.coches.length > 0? concesionario.coches[concesionario.coches.length - 1].id + 1: 1,
-      ...request.body,
-    };
-    concesionarios.coches.push(nuevoCoche);
-    response.json({ message: "ok" });
-  });
+  const concesionarioId = parseInt(request.baseUrl.split("/").slice(-2, -1)[0]);
+  const concesionario = concesionarios.find((c) => c.id === concesionarioId);
+  const nuevoCoche = {
+    id:
+      concesionarios.coches.length > 0
+        ? concesionario.coches[concesionario.coches.length - 1].id + 1
+        : 1,
+    ...request.body,
+  };
+  concesionarios.coches.push(nuevoCoche);
+  response.json({ message: "ok" });
+});
+
+router.get("/:id", (request, response) => {
+  const concesionarioId = parseInt(request.baseUrl.split("/").slice(-2, -1)[0]);
+  const concesionario = concesionarios.find((c) => c.id === concesionarioId);
+  const cocheId = parseInt(request.params.id);
+  const coche = concesionario.listado_coches.find((c) => c.id === cocheId);
+  response.json({coche});
+});
 
 module.exports = router;
