4752a19da5d71277345db127b2cf2fa52a59a96d
Francisco Gaspar Audino Re <faudre755@g.educaand.es>
2025-01-22 22:07:00 +0100

    conexion a mongoDB

--- index.js
+++ index.js
@@ -7,10 +7,15 @@
 
 // Importamos las bibliotecas necesarias.
 // Concretamente el framework express.
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
+
+require("dotenv").config();
 
 // Inicializamos la aplicación
 const app = express();
+
+const uri = process.env.MONGODB;
 
 // Indicamos que la aplicación puede recibir JSON (API Rest)
 app.use(express.json());
@@ -22,6 +27,31 @@ const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Servidor desplegado en puerto: ${port}`);
 });
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 // Definimos una estructura de datos
 // (temporal hasta incorporar una base de datos)
